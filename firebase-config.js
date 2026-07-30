a9ba23220fded03476938fd4761493fff63a6dc6
krkubica <kubica.k@icloud.com>
2026-07-31 00:10:25 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // UZUPEŁNIJ ten plik danymi z Firebase Console → Project settings → Your apps → Web app.
 // Te dane nie są hasłem. Zostaną widoczne w kodzie strony.
 export const firebaseConfig = {
-  apiKey: "WKLEJ_TUTAJ_API_KEY",
-  authDomain: "WKLEJ_TUTAJ_AUTH_DOMAIN",
-  projectId: "WKLEJ_TUTAJ_PROJECT_ID",
-  storageBucket: "WKLEJ_TUTAJ_STORAGE_BUCKET",
-  messagingSenderId: "WKLEJ_TUTAJ_MESSAGING_SENDER_ID",
-  appId: "WKLEJ_TUTAJ_APP_ID"
+  apiKey: "AIzaSyCUjHXNj8xHvBlqT8AodxKyIk6z1EwMXwk",
+  authDomain: "wesele-patrycji-i-krzysztofa.firebaseapp.com",
+  projectId: "wesele-patrycji-i-krzysztofa",
+  storageBucket: "wesele-patrycji-i-krzysztofa.firebasestorage.app",
+  messagingSenderId: "392559459625",
+  appId: "1:392559459625:web:19d57f055d3254cd6ecb38"
 };
 
 // Wpisz e-mail, którym będziesz logować się do panelu admin.html.
-export const ADMIN_EMAIL = "TWÓJ_EMAIL_ADMINISTRATORA";
+export const ADMIN_EMAIL = "kubica.k@icloud.com";
